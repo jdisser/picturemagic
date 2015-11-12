5e6f8b7a501f3cad1f31275b77f2e1d52e57b1cc
jrdisser <jimdisser@altocirrusapps.com>
2015-11-12 16:16:47 -0500

    Established working album pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,35 +1,55 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
-  # GET /pictures
+  before_filter :get_album  #JRD111115
+  # GET album/id/pictures
   # GET /pictures.json
   def index
-    @pictures = Picture.all
+    @pictures = @album.pictures   #JRD111115
+
+    respond_to do |format|
+      format.html #index.html.erb
+      format.json { render json: @pictures}
+    end
   end
 
-  # GET /pictures/1
+  # GET album/id/pictures/1
   # GET /pictures/1.json
   def show
+    @picture = @album.pictures.find(params[:id])    #JRD111115
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @picture}
+    end
   end
 
-  # GET /pictures/new
+  # GET album/id/pictures/new
   def new
-    @picture = Picture.new
+    @album = Album.find(params[:album_id])
+    @picture = @album.pictures.build                #JRD111115
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @picture}
+    end
   end
 
-  # GET /pictures/1/edit
+  # GET album/id/pictures/1/edit
   def edit
+    @picture = @album.pictures.find(params[:id])    #JRD111115
   end
 
   # POST /pictures
   # POST /pictures.json
   def create
-    @picture = Picture.new(picture_params)
+    #@picture = Picture.new(picture_params)
+    @picture = @album.pictures.build(picture_params)  #JRD111115
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
+        format.html { redirect_to ([@album, @picture]), notice: 'Picture was successfully created.' }
+        format.json { render :show, status: :created, location: @picture}
       else
         format.html { render :new }
         format.json { render json: @picture.errors, status: :unprocessable_entity }
@@ -37,12 +57,14 @@ class PicturesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /pictures/1
+  # PATCH/PUT album/id/pictures/1
   # PATCH/PUT /pictures/1.json
   def update
+    @picture = @album.pictures.find(params[:id])    #JRD111115
+
     respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+      if @picture.update_attributes(params[:picture])
+        format.html { redirect_to album_pictures_url(@album), notice: 'Picture was successfully updated.' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -54,9 +76,10 @@ class PicturesController < ApplicationController
   # DELETE /pictures/1
   # DELETE /pictures/1.json
   def destroy
+    @picture = @album.pictures.find(params[:id])    #JRD111115
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to album_pictures_url(@album), notice: 'Picture was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,6 +88,12 @@ class PicturesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_picture
       @picture = Picture.find(params[:id])
+    end
+
+    #get album
+
+    def get_album
+      @album = Album.find(params[:album_id])    #added JRD111115
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
