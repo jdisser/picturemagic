14241616bf517624b084dc2e2a2653926b00deeb
jrdisser <jimdisser@altocirrusapps.com>
2015-11-16 22:51:31 -0500

    added not null constraint to email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117034127) do
+ActiveRecord::Schema.define(version: 20151117034748) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "name"
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 20151117034127) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "email"
+    t.string   "email",           null: false
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
